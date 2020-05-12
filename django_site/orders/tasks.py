6217eafe8d8c85c5b20bb50d483760307982b474
Vadim <vadik654321@gmail.com>
2020-05-12 18:08:13 +0300

    heroku celery

--- django_site/orders/tasks.py
+++ django_site/orders/tasks.py
@@ -9,8 +9,9 @@ from my_site.celery import app
 @app.task
 def order_created(pk):
     order = Order.objects.get(pk=pk)
+    order_item = OrderItem.objects.all()
     subject = f'Первый Компьютерный супермаркет. Онлайн заказ №: {order.pk}'
-    message = f'Номер заказа \n Заказ:  \n Количество:'
+    message = f'Номер заказа {order.pk}\n Заказ: {order_item.item}\n Количество: {order_item.quantity}'
     mail_sent = send_mail(
-        subject, message, 'vadik654321@gmail.com', ['vadik654321@gmail.com'])
+        subject, message, 'vadik654321@gmail.com', [order.email])
     return mail_sent
