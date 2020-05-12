fbe5dcd8d385ec912e3d0c13d01f43f75bc63dbc
Vadim <vadik654321@gmail.com>
2020-05-12 16:53:46 +0300

    heroku celery

--- django_site/orders/views.py
+++ django_site/orders/views.py
@@ -8,7 +8,6 @@ from time import sleep
 def order_create(request):
     order_obj = Order.objects.get(user=request.user, ordered=False)
     form = OrderCreationForm()
-    order_created.delay(order_obj.pk)
     if request.method == 'POST':
         form = OrderCreationForm(request.POST, instance=order_obj)
         if form.is_valid():
@@ -19,6 +18,7 @@ def order_create(request):
             order_items.update(ordered=True)
             for item in order_items:
                 item.save()
+            order_created.delay(order_obj.pk)
             context = {'form': new_order, 'order_obj': order_obj}
             return render(request, 'order_created.html', context)
     else:
