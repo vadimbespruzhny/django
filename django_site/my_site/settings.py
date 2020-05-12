7555a9df2015ed5c3cee6c5602d5e442129bc6e1
Vadim <vadik654321@gmail.com>
2020-05-12 16:32:48 +0300

    heroku celery

--- django_site/my_site/settings.py
+++ django_site/my_site/settings.py
@@ -167,7 +167,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'vadik654321@gmail.com'
-EMAIL_HOST_PASSWORD = 'mis129ra'
+EMAIL_HOST_PASSWORD = 'oayrvtjkpqdvgdqh'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 SERVER_EMAIL = EMAIL_HOST_USER
