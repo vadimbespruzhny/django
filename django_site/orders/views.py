72c8bd17709cb1b4a94f69371426c3d26f8d0b93
Vadim <vadik654321@gmail.com>
2020-05-12 17:58:36 +0300

    heroku celery

--- django_site/orders/views.py
+++ django_site/orders/views.py
@@ -18,7 +18,7 @@ def order_create(request):
             order_items.update(ordered=True)
             for item in order_items:
                 item.save()
-            order_created.delay(new_order.pk)
+            order_created.delay(order_obj.pk)
             context = {'form': new_order, 'order_obj': order_obj}
             return render(request, 'order_created.html', context)
     else:
