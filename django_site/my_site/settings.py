fa40a99d6cf3166734d4a3757fcbbd63f6ff41a0
Vadim <vadik654321@gmail.com>
2020-05-19 15:33:40 +0300

    Добавлено управление курсом доллара

--- django_site/my_site/settings.py
+++ django_site/my_site/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'shrouded-mesa-89939.herokuapp.com']
 
 
 # REDIS related settings
