2759acc72530cee36ad45d37db6de37b0a3aa74d
Vadim <vadik654321@gmail.com>
2020-05-21 18:34:53 +0300

    deploy

--- django_site/my_site/settings.py
+++ django_site/my_site/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import dj_database_url
 import os
 import smtplib
-
-# import django_heroku
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
